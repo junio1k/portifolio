3d99baebb2f36fcdd6df37bbbf3febd8e5b7f419
Celso Junio Ferandes <juniocelso2@gmail.com>
2024-05-12 13:38:00 -0300

    Mudanças no menu

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 
 let navBotao = document.querySelector(".nav-botao");
 let botaoFechar = document.querySelector(".botao-fechar")
-
+let navegacao = document.getElementById("navegacao");
 navBotao.addEventListener("click", function(){
-    console.log("ok")
+    navegacao.style.width = "78%";
+    navegacao.style.opacity = "98%";
+
 })
 botaoFechar.addEventListener("click", function(){
-    window.alert("tudo certo")
+    document.getElementById("navegacao").style.width = "0%"
 })
