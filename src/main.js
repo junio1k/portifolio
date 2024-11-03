a4ef22b51a5775091b505779786f722a1cb39f2d
Celso Junio <88014260+junio1k@users.noreply.github.com>
2024-11-03 19:25:20 -0300

    Css
    Alteração da cor

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 let navBotao = document.querySelector(".nav-botao");
 let botaoFechar = document.querySelector(".botao-fechar")
 let navegacao = document.getElementById("navegacao");
+let downloadCurriculum = document.getElementById("download_curriculum");
 navBotao.addEventListener("click", function(){
     navegacao.style.width = "78%";
     navegacao.style.opacity = "98%";
@@ -10,3 +11,23 @@ navBotao.addEventListener("click", function(){
 botaoFechar.addEventListener("click", function(){
     document.getElementById("navegacao").style.width = "0%"
 })
+
+
+// download curriculo
+
+function download () {
+    /*var pathWay = "./curriculo/currículo.pdf";
+    
+    // criar um link 
+    let a = document.createElement("a");
+    a.classList.add("download_curriculum");
+
+    a.href = pathWay;
+    a.download = pathWay.split("./curriculo/currículo.pdf").pop();
+
+    document.body.appendChild(a);*/
+    for(let i = 0; i <= 4; i++){
+        console.log(i);
+    }
+
+}
